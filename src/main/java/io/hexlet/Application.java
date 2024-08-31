ef343c289e00c110cf9dbd4bd72c3c5a3ed63ed0
Sergey Breusov <bsa2609@yandex.ru>
2024-08-31 18:47:25 +0300

    use try-with-resources

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -7,6 +7,37 @@ public class Application {
     // Нужно указывать базовое исключение,
     // потому что выполнение запросов может привести к исключениям
     public static void main(String[] args) throws SQLException {
+        // Соединение с базой данных тоже нужно отслеживать
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT,"
+                + " username VARCHAR(255), phone VARCHAR(255))";
+
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
+
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789'),"
+                + " ('bob', '111-22-33'), ('mike', '333-44-55')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
+
+            var sql3 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+        }
+     }
+
+    /*
+    // Нужно указывать базовое исключение,
+    // потому что выполнение запросов может привести к исключениям
+    public static void main(String[] args) throws SQLException {
         // Создаем соединение с базой в памяти
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
@@ -38,4 +69,5 @@ public class Application {
         // Закрываем соединение
         conn.close();
     }
+     */
 }
